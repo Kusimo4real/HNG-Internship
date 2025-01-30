f697b138920834cd13695a574b4bccb78642554e
Kusimo4real <abdulsemiukusimo@gmail.com>
2025-01-30 04:56:10 +0100

    a public api to retreive basic information

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from datetime import datetime
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+        )
+
+@app.get("/")
+def read_root():
+    ISO = datetime.now().isoformat() + 'Z'
+    return {
+            "email": "abdulsemiukusimo@gmail.com",
+            "current_datetime": ISO,
+            "github_url": "https://github.com/Kusimo4real/HNG-Internship.git"
+            }
